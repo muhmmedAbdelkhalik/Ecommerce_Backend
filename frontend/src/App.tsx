d15de6f360dbf5bacbd0a7d88bca5b9b316468f0
M.Abdelkhalik <muhmmedabdelkhalik@gmail.com>
2025-12-17 02:03:04 +0200

    Add Navbar component and update dependencies: integrate @mui/icons-material for icons in the frontend, and adjust App and main files for new structure.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home";
 import LoginPage from "./pages/login";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
+        
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
